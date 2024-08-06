bd7c31072b8b99949e8fad09828f0a0ad76805e9
abhishekjoe.15@gmail.com <abhishekjoe.15@gmail.com>
2024-08-06 11:15:41 +0530

    Updates

---
+++ precache-manifest.51f55878f5ff1f24dc678098b1e5b622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40ed61c78b6e2a14be0bd7e50318640",
+    "url": "/onlinebookstore/index.html"
+  },
+  {
+    "revision": "89337a3480d93e178830",
+    "url": "/onlinebookstore/static/css/main.77a00d40.chunk.css"
+  },
+  {
+    "revision": "51e8a79f7d763742e222",
+    "url": "/onlinebookstore/static/js/2.57c7edad.chunk.js"
+  },
+  {
+    "revision": "1ac23edd848720c87dd7eac90d67e14d",
+    "url": "/onlinebookstore/static/js/2.57c7edad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3fa8d4a1d1729d4ba8",
+    "url": "/onlinebookstore/static/js/3.9eb53bd2.chunk.js"
+  },
+  {
+    "revision": "89337a3480d93e178830",
+    "url": "/onlinebookstore/static/js/main.b73b0034.chunk.js"
+  },
+  {
+    "revision": "2ebb0ff750ea55893002",
+    "url": "/onlinebookstore/static/js/runtime-main.88160e7b.js"
+  }
+]);
